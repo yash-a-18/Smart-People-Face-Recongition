0df16479c39dd7671bf052f6099f9ac753de9473
yash-a-18 <yashamethiya2001@gmail.com>
2021-12-15 10:46:27 +0530

    Made server using Flask

---
+++ sever/wavelet.py
@@ -0,0 +1,25 @@
+import pywt
+import cv2
+import numpy as np
+
+def w2d(img, mode="haar", level=1):
+    imArray = img
+    # Datatype conversion
+    # Convert to grayscale
+    imArray = cv2.cvtColor(imArray, cv2.COLOR_BGR2GRAY)
+    # convert to float
+    imArray = np.float32(imArray)
+    imArray /= 255
+    #compute coeeficients
+    coeffs = pywt.wavedec2(imArray, mode, level = level)
+    
+    #process coefficients
+    coeffs_H = list(coeffs)
+    coeffs_H[0] *= 0
+    
+    # reconstruction
+    imArray_H = pywt.waverec2(coeffs_H, mode)
+    imArray_H *= 255
+    imArray_H = np.uint8(imArray_H)
+    
+    return imArray_H
